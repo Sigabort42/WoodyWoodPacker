d3601a036fd8bd58916d77ecf97c9dccf1bc27bb
Kali <kali@kali>
2021-01-05 20:01:44 -0500

    printf ...woody...

--- payload.s
+++ payload.s
@@ -2,7 +2,7 @@ section .text
 	global _start
 _ender:
 	call _starter
-	msg db '...WOODY...',0xA,0
+	msg db '...WOODY...',0x0a,0
 	
 _start:
 	jmp _ender
@@ -10,7 +10,7 @@ _start:
 _starter:
 	pop rsi
 	mov al, 0x1
-	mov dl, 0x1
+	mov rdi, 0x1
 	mov rdx, 0xd
 	syscall
 	mov rdx, 0x0
